718dd9577efa56b31b8c5853b325bf69f7fcdae1
ajoydebnath22 <ajoydebnath22@gmail.com>
2020-08-20 22:25:34 +0530

    Rest Controller Implement

--- src/main/java/com/ajoy/eureka/client/SpringBootSonerApplication.java
+++ src/main/java/com/ajoy/eureka/client/SpringBootSonerApplication.java
@@ -3,8 +3,10 @@ package com.ajoy.eureka.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootSonerApplication {
 
 	public static void main(String[] args) {
@@ -12,7 +14,7 @@ public class SpringBootSonerApplication {
 	}
 
 	
-	@GetMapping("/message")
+	@GetMapping(value = "message")
 	public String welcomeMessage() {
 		return "Message Spring Boot App Soner Message";
 	}
