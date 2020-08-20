a494ffc2844141a0bd40ce89af2adb211773152b
ajoydebnath22 <ajoydebnath22@gmail.com>
2020-08-20 20:54:26 +0530

    First Commit

---
+++ src/main/java/com/ajoy/eureka/client/SpringBootSonerApplication.java
@@ -0,0 +1,13 @@
+package com.ajoy.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSonerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajoy/eureka/client/SpringBootSonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajoy.eureka.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
