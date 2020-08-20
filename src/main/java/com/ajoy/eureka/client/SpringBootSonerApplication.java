2fb813ba519bbb6705f46a96e6c788767b33bb03
ajoydebnath22 <ajoydebnath22@gmail.com>
2020-08-20 21:28:28 +0530

    Request mapping implemented

--- src/main/java/com/ajoy/eureka/client/SpringBootSonerApplication.java
+++ src/main/java/com/ajoy/eureka/client/SpringBootSonerApplication.java
@@ -2,6 +2,7 @@ package com.ajoy.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootSonerApplication {
@@ -10,4 +11,10 @@ public class SpringBootSonerApplication {
 		SpringApplication.run(SpringBootSonerApplication.class, args);
 	}
 
+	
+	@GetMapping("/message")
+	public String welcomeMessage() {
+		return "Message Spring Boot App Soner Message";
+	}
+	
 }
