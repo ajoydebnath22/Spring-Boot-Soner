a3771f8a8a64366ee4175ce429fce2870bf0a82f
ajoydebnath22 <ajoydebnath22@gmail.com>
2020-08-21 12:24:42 +0530

    SH file change

--- kill-boot.sh
+++ kill-boot.sh
@@ -1,4 +1,4 @@
-PID=`ps -eaf | grep "java -jar spring-boot-soner.jar" | grep -v grep | awk '{print $2}'`
+PID=`ps -eaf | grep "java -jar" | grep -v grep | awk '{print $2}'`
 if [[ "" !=  "$PID" ]]; then
   echo "found $PID and killing it!!"
   kill -9 $PID
